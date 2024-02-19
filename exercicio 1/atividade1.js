5736a7023dbc067d996fe67267f1a0e50610e0f2
Joao gui <joao.hanau@escola.pr.gov.br>
2024-02-19 09:45:58 -0300

    mandando arquivos

---
+++ exercicio 1/atividade1.js
@@ -0,0 +1,7 @@
+var inicio = 1;
+var final = 10;
+
+for(inicio;inicio<=final;inicio++){
+    alert(inicio);  
+   
+}
---
+++ exercico 4/atividade4.js
@@ -0,0 +1,9 @@
+const numero2 = -5;
+
+if (numero2>0)  {
+    alert( `o numero  ${numero2} é positivo` );
+} else if(numero2<0){
+    alert( `o numero  ${numero2} é negativo` );
+} else {
+    alert( `o numero ${numero2} é zero`);
+} 
